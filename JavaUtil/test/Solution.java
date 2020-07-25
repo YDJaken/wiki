522636f9933ee25cc11ddc0e737ad3036cc95c5f
YD <YD@DYJA.com>
2020-07-25 21:55:08 +0800

    update

--- JavaUtil/test/Solution.java
+++ JavaUtil/test/Solution.java
@@ -105,6 +105,143 @@ class Solution {
 		}
 	}
 
+    /*
+	 * For strings S and T, we say "T divides S" if and only if S = T + ... + T  (T
+	 * concatenated with itself 1 or more times)
+	 *
+	 * Return the largest string X such that X divides str1 and X divides str2.
+	 *
+	 * Example 1:
+	 * Input: str1 = "ABCABC", str2 = "ABC" Output: "ABC"
+	 * Example 2:
+	 * Input: str1 = "ABABAB", str2 = "ABAB" Output: "AB"
+	 * Example 3:
+	 * Input: str1 = "LEET", str2 = "CODE" Output: ""  
+	 *
+	 * Note:
+	 *
+	 * 1 <= str1.length <= 1000
+	 * 1 <= str2.length <= 1000
+	 * str1[i] and str2[i] are English uppercase letters.
+	 *
+	 * 	来源：力扣（LeetCode）
+	 * 	链接：https://leetcode-cn.com/problems/greatest-common-divisor-of-strings
+	 * 	著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+	 */
+    // 解法1:
+	 public String gcdOfStrings(String str1, String str2) {
+	        int length1 = str1.length();
+	        int length2 = str2.length();
+	        do {
+	            if (length1 < length2) {
+	                if (!str2.endsWith(str1)) {
+	                    return "";
+	                }
+	                length2 -= length1;
+	                if (length2 == 0) {
+	                    return str1;
+	                }
+	                str2 = str2.substring(0, length2);
+	            } else {
+	                if (!str1.endsWith(str2)) {
+	                    return "";
+	                }
+	                length1 -= length2;
+	                if (length1 == 0) {
+	                    return str2;
+	                }
+	                str1 = str1.substring(0, length1);
+	            }
+	        } while (true);
+	}
+	// 解法2:
+	public String gcdOfStrings(String str1, String str2) {
+		if(!(str1 + str2).equals(str2 + str1)){
+			return "";
+		}
+
+		return str1.substring(0,findGCD(str1.length(),str2.length()));
+	}
+
+	private int findGCD(int a, int b) {
+		return b == 0? a : findGCD(b , a % b);
+	}
+
+
+	/*
+	 * Given a non-empty special binary tree consisting of nodes with the
+	 * non-negative value, where each node in this tree has exactly two or zero
+	 * sub-node. If the node has two sub-nodes, then this node's value is the
+	 * smaller value among its two sub-nodes. More formally, the property root.val =
+	 * min(root.left.val, root.right.val) always holds.
+	 *
+	 * Given such a binary tree, you need to output the second minimum value in the
+	 * set made of all the nodes' value in the whole tree.
+	 *
+	 * If no such second minimum value exists, output -1 instead.
+	 *
+	 * Example 1:
+	 *
+	 * Input: 2 / \ 2 5 / \ 5 7
+	 *
+	 * Output: 5 Explanation: The smallest value is 2, the second smallest value is
+	 * 5.
+	 *
+	 *
+	 * Example 2:
+	 *
+	 * Input: 2 / \ 2 2
+	 *
+	 * Output: -1 Explanation: The smallest value is 2, but there isn't any second
+	 * smallest value.
+	 */
+	 class TreeNode {
+     		int val;
+     		TreeNode left;
+     		TreeNode right;
+
+     		TreeNode() {
+     		}
+
+     		TreeNode(int val) {
+     			this.val = val;
+     		}
+
+     		TreeNode(int val, TreeNode left, TreeNode right) {
+     			this.val = val;
+     			this.left = left;
+     			this.right = right;
+     		}
+     }
+	// 树节点遍历
+	public int findSecondMinimumValue(TreeNode root) {
+		if (root == null || root.left == null) {
+			return -1;
+		}
+		return findValue(root, root.val);
+	}
+
+	private int findValue(TreeNode node, int value) {
+		if (node.val != value) {
+			return node.val;
+		} else {
+			if(node.left == null) {
+				return -1;
+			}
+			int left = findValue(node.left, value);
+			int right = findValue(node.right, value);
+			if(left == -1) {
+				return right;
+			}
+			if(right == -1) {
+				return left;
+			}
+			return Math.min(left, right);
+		}
+	}
+
+
+
 /*
 	 * 有效括号字符串 定义：对于每个左括号，都能找到与之对应的右括号，反之亦然。详情参见题末「有效括号字符串」部分。
 	 *
