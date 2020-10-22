03165715ee5a8480432e08113420db4bfe0db34c
YDJaken <43409420+YDJaken@users.noreply.github.com>
2020-10-22 20:51:14 +0800

    Update SolutionTest.java

--- JavaUtil/test/SolutionTest.java
+++ JavaUtil/test/SolutionTest.java
@@ -6,48 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
-class LogSystem {
-    private HashMap<String, Integer> map;
-    public LogSystem() {
-        this.map = new HashMap<>();
-    }
-    
-    public void put(int id, String timestamp) {
-        this.map.put(timestamp,id);
-    }
-    
-    public List<Integer> retrieve(String s, String e, String gra) {
-        List<Integer> ret =  new LinkedList<>();
-        int length = 0;
-        if(gra.equals("Year")){
-            length = 4;
-        }else if (gra.equals("Month")){
-            length = 7;
-        }else if (gra.equals("Day")){
-            length = 10;
-        }else if (gra.equals("Hour")){
-            length = 13;
-        }else if (gra.equals("Minute")){
-            length = 18;
-        }else if (gra.equals("Second")){
-            length = 19;
-        }
-        String startStr = s.substring(0,length);
-        String endStr = e.substring(0,length);
-        for(Map.Entry<String,Integer> entry : this.map.entrySet()){
-            String tmpStr = entry.getKey().substring(0,length);
-            if(tmpStr.compareTo(startStr)<0){
-                continue;
-            }
-            if(tmpStr.compareTo(endStr)>0){
-                continue;
-            }
-            ret.add(entry.getValue());
-        }
-        return ret;
-    }
-}
-
 
 	@Test
 	void testMatch() {
